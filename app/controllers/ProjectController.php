284f55fc0d079d508c2a3924bc1343055d52ce38
jmramos02 <ramosjosemari@gmail.com>
2014-11-22 16:17:21 +0800

    nag add ng links

--- app/controllers/ProjectController.php
+++ app/controllers/ProjectController.php
@@ -2,6 +2,9 @@
 
 class ProjectController extends BaseController{
 	public function showProjects(){
-		return View::make('project');
+		$projectUrl = "http://philgeps.cloudapp.net:5000/api/action/datastore_search?resource_id=539525df-fc9a-4adf-b33d-04747e95f120";
+		$str = get_object_vars(json_decode(file_get_contents($projectUrl)));
+		//return View::make('project');
+		return var_dump($str['result']);
 	}
 }
